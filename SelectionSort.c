357ed090e86446a5ec5fdb4357e54476603dd464
gourisnair <gouri.gopan@gmail.com>
2018-09-21 12:22:26 +0530

    Selection sort

---
+++ SelectionSort.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+  int i, j, size;
+  printf("Enter size of array: ");
+  scanf("%d", &size);
+  int a[size];
+  printf("Enter array\n");
+  for(i = 0; i < size; i++) {
+    scanf("%d", &a[i]);
+  }
+
+  int temp;
+  for(i = 0; i < size; i++) {
+    for(j = i + 1; j < size; j++) {
+      if(a[i] > a[j]) {
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
+    }
+  }
+  for(i = 0; i < size; i++) {
+    printf("%d", a[i]);
+  }
+}
